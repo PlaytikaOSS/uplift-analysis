14bddc2bb5944e994d8c34862a400d1993319a86
DvirB </6j!>
2022-01-20 18:16:22 +0200

    add some comments for the tests

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -6,18 +6,22 @@ from uplift_analysis import evaluation, data
 
 def test_initialization():
     """Test the initialization of the Evaluator"""
+
+    # initialization does not comply with expected and permitted types
     with pytest.raises(Exception) as _:
         ev = evaluation.Evaluator(sets_config={'response_field': 5})
 
-    # this should succeed
+    # should succeed
     ev = evaluation.Evaluator(sets_config={'response_field': 'response'})
 
 
 def test_single_eval_set():
     """Test the usage of the Evaluator on a single EvalSet object"""
-    siz = 1000
 
-    for num_actions in [2, 5]:
+    siz = 1000  # sample size
+
+    for num_actions in [2, 5]:  # run once for binary action, and once for multiple actions
+        # scored dataframe with random actions, random scores, and random responses
         scored_df = pd.DataFrame(
             {
                 'recommended_action': np.random.randint(num_actions, size=siz),
@@ -27,32 +31,39 @@ def test_single_eval_set():
             }
         )
 
+        # create a corresponding EvalSet object
         eval_set = data.EvalSet(df=scored_df,
                                 observed_action_field='observed_action',
                                 response_field='response',
                                 score_field='score',
                                 proposed_action_field='recommended_action',
                                 control_indicator=0)
+        # and an Evaluator
         evaluator = evaluation.Evaluator()
+
+        # test input verification of the evaluator
         with pytest.raises(Exception) as _:
             evaluator.evaluate_set(input_set=None)
 
+        # should succeed
         eval_res, summary = evaluator.eval_and_show(data=eval_set, title_suffix='single', show_random=True,
                                                     num_random_rep=4)
-
+        # verify returned types
         assert isinstance(eval_res, data.EvalSet)
         assert isinstance(summary, dict)
 
+        # test input verification of the evaluator visualization method
         with pytest.raises(Exception) as _:
             evaluator.visualize(eval_res=np.array([1, 2]))
 
+        # specified charts must be listed as part of the Evaluator class
         with pytest.raises(Exception) as _:
             evaluator.visualize(eval_res=eval_res, specify=['Untitled'])
 
         # this should succeed
         evaluator.visualize(eval_res=eval_res, specify=['agreements'])
 
-        # this should fail
+        # does not accept EvalSets which were not evaluated
         eval_res.is_evaluated = False
         with pytest.raises(Exception) as _:
             evaluator.visualize(eval_res=eval_res, specify=['agreements'])
@@ -61,8 +72,14 @@ def test_single_eval_set():
 def test_multiple_eval_set():
     """Test the usage of the Evaluator on multiple EvalSet objects"""
 
+    # create a collection of EvalSets
     eval_sets = dict()
+
+    # Each EvalSet might be different in its:
+    # - number of actions
+    # - sample size
     for num_actions, siz, name in zip([2, 5], [1000, 2000], ['Eval_A', 'Eval_B']):
+        # create a scored dataframe with random actions, random scores, and random responses
         scored_df = pd.DataFrame(
             {
                 'recommended_action': np.random.randint(num_actions, size=siz),
@@ -72,6 +89,7 @@ def test_multiple_eval_set():
             }
         )
 
+        # create a corresponding EvalSet object
         eval_sets[name] = data.EvalSet(df=scored_df,
                                        observed_action_field='observed_action',
                                        response_field='response',
@@ -79,19 +97,24 @@ def test_multiple_eval_set():
                                        proposed_action_field='recommended_action',
                                        control_indicator=0)
 
+    # a single Evaluator to use on the collection of EvalSet objects
     evaluator = evaluation.Evaluator()
+
+    # test input verification of the evaluator evaluate_multiple() function
     with pytest.raises(Exception) as _:
         evaluator.evaluate_multiple(input_set=None)
 
+    # a valid call
     eval_res, summary = evaluator.eval_and_show(data=eval_sets, title_suffix='multiple', average=True)
 
+    # verify returned types
     assert isinstance(eval_res, dict)
     assert isinstance(summary, pd.DataFrame)
 
     # this should succeed
     evaluator.visualize(eval_res=eval_res, specify=['agreements'])
 
-    # this should fail
+    # does not accept EvalSets which were not evaluated
     eval_res['Eval_A'].is_evaluated = False
     with pytest.raises(Exception) as _:
         evaluator.visualize(eval_res=eval_res, specify=['agreements'])
